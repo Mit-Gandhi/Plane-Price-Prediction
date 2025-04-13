f6a2ec4246410739ca5c78e17c287f332af37e05
Mit Gandhi <141493846+Mit-Gandhi@users.noreply.github.com>
2025-04-13 23:32:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://plane-price-prediction.onrender.com/"
+        "https://plane-price-prediction.onrender.com",  # Your deployed frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
